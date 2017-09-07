8307c71396a85f4be93cfbbebeabf5303a5b5293
Thomas <thomasvrielink92@gmail.com>
2017-09-07 10:30:54 +0200

    anomaly, buffers and filter added

--- app/src/main/java/com/example/noblenotebooklouis/challenge1/Filter.java
+++ app/src/main/java/com/example/noblenotebooklouis/challenge1/Filter.java
@@ -5,4 +5,37 @@ package com.example.noblenotebooklouis.challenge1;
  */
 
 public class Filter {
+
+    int length;
+
+    public Filter(int length) {
+        this.length = length;
+
+    }
+
+    public double[] filtering(double[] data) {
+        double[] filtered = new double[data.length];
+        for (int i = 0; i < data.length; i++) {
+
+            double value = data[0];
+            if (i < length) {
+                for (int j = 0; j < i; j++) {
+                    value = value + data[j+1];
+                }
+                value = value/i;
+            } else {
+                for (int j = i-length; j < i; j++) {
+                    value = value + data[j+1];
+                }
+                value = value/length;
+            }
+
+            filtered[i] = value;
+        }
+
+        return filtered;
+
+    }
+
+
 }
